d4a13e668e90da777d7dfab6a55d134f88ff4d0d
Pratham <prathamlashkari841@gmail.com>
2024-06-05 17:38:30 +0530

    Setup backend

---
+++ server/src/main/java/com/CabBook/Cab/CabApplication.java
@@ -0,0 +1,15 @@
+package com.CabBook.Cab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabApplication.class, args);
+		System.out.println("STarted");
+	
+	}
+
+}
