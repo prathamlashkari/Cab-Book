ca0d9736b6ff85f64c44612cc94e4806e83abedc
Pratham <prathamlashkari841@gmail.com>
2024-06-06 13:27:53 +0530

    Anther

--- server/src/main/java/com/CabBook/Cab/config/AppConfig.java
+++ server/src/main/java/com/CabBook/Cab/config/AppConfig.java
@@ -1,21 +1,6 @@
 package com.CabBook.Cab.config;
 
-import java.net.http.HttpRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-
-import io.jsonwebtoken.lang.Arrays;
-import io.jsonwebtoken.lang.Collections;
-import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class AppConfig {
