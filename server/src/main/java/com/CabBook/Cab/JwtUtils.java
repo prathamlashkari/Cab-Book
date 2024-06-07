33b03841a550fbb10422e0c6fef644adb3caa117
Pratham <prathamlashkari841@gmail.com>
2024-06-07 14:23:12 +0530

    Setups manager and proivder now creating oncerprerreq

---
+++ server/src/main/java/com/CabBook/Cab/JwtUtils.java
@@ -0,0 +1,9 @@
+package com.CabBook.Cab;
+
+public class JwtUtils {
+  private JwtUtils() {
+  }
+
+  private static final String secretkey = "ksjfniqhwiotkcjxnvklndfoisiowqerijasdnfjsdhfjgknvmeior";
+  private static final String ISSUER = "code-with-Golu";
+}
--- server/src/main/java/com/CabBook/Cab/config/JwtAuthenticationFilter.java
+++ server/src/main/java/com/CabBook/Cab/config/JwtAuthenticationFilter.java
@@ -1,10 +1,13 @@
 package com.CabBook.Cab.config;
 
 import java.io.IOException;
+import java.util.Optional;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import org.springframework.util.StringUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,7 +19,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+
+    Optional<String> jwtToken = getTokenFormRequest(request);
+
+    jwtToken.ifPresent(jwt -> {
+    });
+  }
+
+  private Optional<String> getTokenFormRequest(HttpServletRequest request) {
+    String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+    if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
+      return Optional.of(authHeader.substring(7));
+    }
+    return Optional.ofNullable(null);
   }
 
 }
