7ac9e70fab4ff5ac284be1bf79a5f99ae466e3ec
Pratham <prathamlashkari841@gmail.com>
2024-06-07 14:53:58 +0530

    Ading userdetail service class

--- server/src/main/java/com/CabBook/Cab/JwtUtils.java
+++ server/src/main/java/com/CabBook/Cab/JwtUtils.java
@@ -1,9 +1,62 @@
 package com.CabBook.Cab;
 
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.apache.commons.lang3.time.DateUtils;
+
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class JwtUtils {
   private JwtUtils() {
   }
 
-  private static final String secretkey = "ksjfniqhwiotkcjxnvklndfoisiowqerijasdnfjsdhfjgknvmeior";
+  private static final String SECRET_KEY_STRING = "ksjfniqhwiotkcjxnvklndfoisiowqerijasdnfjsdhfjgknvmeior";
   private static final String ISSUER = "code-with-Golu";
+
+  // Create a SecretKey object from the secret key string
+  private static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(SECRET_KEY_STRING.getBytes());
+
+  public static boolean validateToken(String jwtToken) {
+    return parseToken(jwtToken).isPresent();
+  }
+
+  public static Optional<String> getEmailFromToken(String jwtToken) {
+    var claimsOptional = parseToken(jwtToken);
+    return claimsOptional.map(claims -> claims.get("email", String.class));
+  }
+
+  private static Optional<Claims> parseToken(String jwtToken) {
+    var jwtParser = Jwts.parserBuilder()
+        .setSigningKey(SECRET_KEY)
+        .build();
+    try {
+      Claims claims = jwtParser.parseClaimsJws(jwtToken).getBody();
+      return Optional.of(claims);
+    } catch (JwtException | IllegalArgumentException e) {
+      log.error("Jwt Exception occurred: {}", e.getMessage());
+    }
+    return Optional.empty();
+  }
+
+  public static String generateToken(String email) {
+    var date = new Date();
+    var expiration = DateUtils.addMinutes(date, 10);
+    return Jwts.builder()
+        .setId(UUID.randomUUID().toString())
+        .setIssuer(ISSUER)
+        .setSubject(email)
+        .setIssuedAt(date)
+        .setExpiration(expiration)
+        .signWith(SECRET_KEY)
+        .compact();
+  }
 }
--- server/src/main/java/com/CabBook/Cab/config/JwtAuthenticationFilter.java
+++ server/src/main/java/com/CabBook/Cab/config/JwtAuthenticationFilter.java
@@ -3,9 +3,17 @@ package com.CabBook.Cab.config;
 import java.io.IOException;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.CabBook.Cab.JwtUtils;
 
 import org.springframework.util.StringUtils;
 import jakarta.servlet.FilterChain;
@@ -16,6 +24,9 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+  @Autowired
+  private UserDetailsService userDetailsService;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
@@ -23,6 +34,21 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     Optional<String> jwtToken = getTokenFormRequest(request);
 
     jwtToken.ifPresent(jwt -> {
+
+      if (JwtUtils.validateToken(jwt)) {
+        Optional<String> emailOpt = JwtUtils.getEmailFromToken(jwt);
+
+        if (emailOpt.isPresent()) {
+          String email = emailOpt.get();
+          UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+          UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,
+              null, userDetails.getAuthorities());
+          authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+          SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+      }
     });
   }
 
