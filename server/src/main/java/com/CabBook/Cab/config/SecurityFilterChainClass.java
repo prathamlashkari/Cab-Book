2512001c5b4c2e6bfb17521da00d2ef902e9cb26
Pratham <prathamlashkari841@gmail.com>
2024-06-07 13:44:55 +0530

    Added securiyt

---
+++ server/src/main/java/com/CabBook/Cab/config/SecurityFilterChainClass.java
@@ -0,0 +1,41 @@
+package com.CabBook.Cab.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Configuration
+public class SecurityFilterChainClass {
+
+  @Bean
+  private SecurityFilterChain securityFilterChain(HttpSecurity https) throws Exception {
+    https.csrf(csrfConfig -> csrfConfig.disable());
+
+    https.authorizeHttpRequests(authConfig -> authConfig.requestMatchers("/api/auth**").permitAll()
+        .anyRequest().authenticated());
+    https.cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
+
+      @Override
+      public CorsConfiguration getCorsConfiguration(HttpServletRequest req) {
+        CorsConfiguration cfg = new CorsConfiguration();
+        cfg.setAllowedHeaders(Collections.singletonList("*"));
+        cfg.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+        cfg.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        cfg.addExposedHeader("Authorization");
+        cfg.setMaxAge(3600L);
+        cfg.setAllowCredentials(true);
+        return cfg;
+      }
+    }));
+
+    return https.build();
+  }
+}
