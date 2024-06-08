1425a70eb33d4b7361f37776fcf972bc390a0841
Pratham <prathamlashkari841@gmail.com>
2024-06-08 16:58:36 +0530

    creating dfiver additonal licexe rating class

--- server/src/main/java/com/CabBook/Cab/models/Driver.java
+++ server/src/main/java/com/CabBook/Cab/models/Driver.java
@@ -1,11 +1,27 @@
 package com.CabBook.Cab.models;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.CabBook.Cab.enums.UserRole;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @Document(collection = "drivers")
 public class Driver {
 
+  @Id
+  private String Id;
+
+  private String fullName;
+  private String mobile;
+  private String email;
+  private double rating;
+  private double longitude;
+  private double latitude;
+  private UserRole userRole;
+  private String password;
 }
