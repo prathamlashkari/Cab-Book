61884924564d6a455ab124345486ca1ed33d5fdd
Pratham <prathamlashkari841@gmail.com>
2024-06-08 15:32:49 +0530

    Setup models o fuser

--- server/src/main/java/com/CabBook/Cab/models/User.java
+++ server/src/main/java/com/CabBook/Cab/models/User.java
@@ -1,8 +1,22 @@
 package com.CabBook.Cab.models;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+
+@Data
 @Document(collection = "users")
 public class User {
+
+  @Id
+  private String Id;
+
+  private String fullName;
+  private String email;
+  private String mobile;
+  private String password;
+  private String profilePicture;
+  private String UserRole;
 
 }
