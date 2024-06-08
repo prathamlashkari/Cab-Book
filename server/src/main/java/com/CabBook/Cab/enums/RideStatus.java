9920627bed0e5b09609f0a8162982e69e8df9a50
Pratham <prathamlashkari841@gmail.com>
2024-06-08 18:26:08 +0530

    Adding enums of ridestatus

---
+++ server/src/main/java/com/CabBook/Cab/enums/RideStatus.java
@@ -0,0 +1,9 @@
+package com.CabBook.Cab.enums;
+
+public enum RideStatus {
+  ACCEPTED,
+  REQUESTED,
+  CANCELLED,
+  STARTED,
+  COMPLETED
+}
--- server/src/main/java/com/CabBook/Cab/models/Ride.java
+++ server/src/main/java/com/CabBook/Cab/models/Ride.java
@@ -7,10 +7,14 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.CabBook.Cab.enums.RideStatus;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "")
 public class Ride {
@@ -27,9 +31,13 @@ public class Ride {
   private double destinationLongitude;
   private String pickupArea;
   private String destinationArea;
+  private double distance;
+  private long duration;
+  private double fare;
   private RideStatus status;
   private LocalDateTime starTime;
   private LocalDateTime endTime;
   private int opt;
 
+  private PaymentDetails paymentDetails = new PaymentDetails<>();
 }
