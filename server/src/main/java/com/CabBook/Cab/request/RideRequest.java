cd5809814f765f0626eed05b5065a72499303cb1
Pratham <prathamlashkari841@gmail.com>
2024-06-09 12:34:09 +0530

    Ride requ added

---
+++ server/src/main/java/com/CabBook/Cab/request/RideRequest.java
@@ -0,0 +1,16 @@
+package com.CabBook.Cab.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class RideRequest {
+
+  private double pickupLongitude;
+  private double pickupLatitude;
+  private double destinationLongitude;
+  private double destinationLatitude;
+  private String pickupArea;
+  private String destinationArea;
+}
