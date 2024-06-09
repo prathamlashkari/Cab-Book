c89ccbfdbfe640f4254d8233f4e009f2a153a04a
Pratham <prathamlashkari841@gmail.com>
2024-06-09 12:02:51 +0530

    Added user details class

--- server/src/main/java/com/CabBook/Cab/service/UserDetailSerivceImpl.java
+++ server/src/main/java/com/CabBook/Cab/service/UserDetailSerivceImpl.java
@@ -1,16 +1,41 @@
 package com.CabBook.Cab.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.CabBook.Cab.models.Driver;
+import com.CabBook.Cab.models.User;
+import com.CabBook.Cab.repository.DriverRepository;
+import com.CabBook.Cab.repository.UserRepository;
+
 @Service
 public class UserDetailSerivceImpl implements UserDetailsService {
 
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private DriverRepository driverRepository;
+
   @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    List<GrantedAuthority> authorities = new ArrayList<>();
+    User user = userRepository.findByEmail(email);
+    if (user != null) {
+      return new org.springframework.security.core.userdetails.User(user.getFullName(), user.getPassword(),
+          authorities);
+    }
+    Driver driver = driverRepository.findByEmail(email);
+    if (driver != null) {
+      return new org.springframework.security.core.userdetails.User(driver.getFullName(), driver.getPassword(),
+          authorities);
+    }
     return null;
   }
-
 }
