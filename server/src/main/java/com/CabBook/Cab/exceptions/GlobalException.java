bc4d752acb5c1d2eaeb8000f4bad102965ba2a4d
Pratham <prathamlashkari841@gmail.com>
2024-06-09 12:59:47 +0530

    global excpetion done

--- server/src/main/java/com/CabBook/Cab/exceptions/GlobalException.java
+++ server/src/main/java/com/CabBook/Cab/exceptions/GlobalException.java
@@ -1,8 +1,20 @@
 package com.CabBook.Cab.exceptions;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalException {
+
+  @ExceptionHandler(UserException.class)
+  public ResponseEntity<ErrorDetails> userExceptionHandler(UserException ue, WebRequest req) {
+    ErrorDetails err = new ErrorDetails(ue.getMessage(), req.getDescription(false), LocalDateTime.now());
+    return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+  }
 
 }
