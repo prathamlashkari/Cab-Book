9291347fce5f01e743693af1365971e43390f074
Pratham <prathamlashkari841@gmail.com>
2024-06-09 11:41:51 +0530

    Added vehcicle class

--- server/src/main/java/com/CabBook/Cab/models/Vehicle.java
+++ server/src/main/java/com/CabBook/Cab/models/Vehicle.java
@@ -1,8 +1,22 @@
 package com.CabBook.Cab.models;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+
+@Data
 @Document(collection = "vehicles")
 public class Vehicle {
+
+  @Id
+  private String Id;
+  private String make;
+  private String model;
+  private String year;
+  private String colour;
+  private String capacity;
+  private String licensePlate;
+  private String driverId;
 
 }
