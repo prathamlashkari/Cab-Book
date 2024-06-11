1d6c0c3bd9f0d1b48e9641c76e54bc5a6dfc4fb0
Pratham <prathamlashkari841@gmail.com>
2024-06-11 10:44:54 +0530

    Added annotaions to repos

--- server/src/main/java/com/CabBook/Cab/repository/VehicleRepository.java
+++ server/src/main/java/com/CabBook/Cab/repository/VehicleRepository.java
@@ -1,9 +1,11 @@
 package com.CabBook.cab.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.CabBook.cab.models.Vehicle;
 
+@Repository
 public interface VehicleRepository extends MongoRepository<Vehicle, String> {
 
 }
--- server/src/main/java/com/CabBook/Cab/service/classfile/AuthSerivceImpl.java
+++ server/src/main/java/com/CabBook/Cab/service/classfile/AuthSerivceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.CabBook.cab.enums.UserRole;
 import com.CabBook.cab.exceptions.UserException;
@@ -14,6 +15,7 @@ import com.CabBook.cab.request.LoginRequest;
 import com.CabBook.cab.request.SignupRequest;
 import com.CabBook.cab.service.interfacefile.AuthService;
 
+@Service
 public class AuthSerivceImpl implements AuthService {
 
   @Autowired
