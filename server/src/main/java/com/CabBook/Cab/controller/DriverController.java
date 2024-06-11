8fdd74609e6c84af2fd89566a267f3fafe89d59f
Pratham <prathamlashkari841@gmail.com>
2024-06-11 12:41:59 +0530

    Creating drvier controller

---
+++ server/src/main/java/com/CabBook/Cab/controller/DriverController.java
@@ -0,0 +1,10 @@
+package com.CabBook.cab.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/drivers")
+public class DriverController {
+
+}
