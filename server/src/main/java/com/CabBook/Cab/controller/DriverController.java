701bee9916ad7619c8a5a0e90591446472499101
Pratham <prathamlashkari841@gmail.com>
2024-06-11 12:46:17 +0530

    first crated drvier controller

--- server/src/main/java/com/CabBook/Cab/controller/DriverController.java
+++ server/src/main/java/com/CabBook/Cab/controller/DriverController.java
@@ -1,10 +1,31 @@
 package com.CabBook.cab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.CabBook.cab.exceptions.DriverException;
+import com.CabBook.cab.models.Driver;
+import com.CabBook.cab.service.interfacefile.DriverService;
 
 @RestController
 @RequestMapping("/api/drivers")
 public class DriverController {
+
+  @Autowired
+  private DriverService driverService;
+
+  @GetMapping("/profile")
+  public ResponseEntity<Driver> getReqDriverProfile(@RequestHeader("Authorization") String jwt) throws DriverException {
+    Driver driver = driverService.getReqDriverProfile(jwt);
+    if (driver == null) {
+      throw new DriverException("Drive not found");
+    }
+    return new ResponseEntity<>(driver, HttpStatus.OK);
+  }
 
 }
