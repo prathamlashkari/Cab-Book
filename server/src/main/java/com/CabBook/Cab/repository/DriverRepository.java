573c341ffc7abe34796990ef06cbc682da686a55
Pratham <prathamlashkari841@gmail.com>
2024-06-11 10:14:33 +0530

    Added extra query for driver repso

--- server/src/main/java/com/CabBook/Cab/repository/DriverRepository.java
+++ server/src/main/java/com/CabBook/Cab/repository/DriverRepository.java
@@ -1,11 +1,22 @@
 package com.CabBook.cab.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import com.CabBook.cab.models.Ride;
 
 import com.CabBook.cab.models.Driver;
 
 @Repository
 public interface DriverRepository extends MongoRepository<Driver, String> {
   public Driver findByEmail(String email);
+
+  @Query("{ 'rides.status': 'REQUESTED', 'rides.driverId': :#{#driverId} }")
+  List<Ride> getAllocatedRides(@Param("driverId") String driverId);
+
+  @Query("{ 'rides.status': 'COMPLETED', 'rides.driverId': :#{#driverId} }")
+  List<Ride> getCompleteRides(@Param("driverId") String driverId);
 }
