40bb8d77a371d26e58021a54e4ea3684585512e0
Pratham <prathamlashkari841@gmail.com>
2024-06-12 09:26:59 +0530

    Added userservice interface and its methods

--- server/src/main/java/com/CabBook/Cab/controller/Ridecontroller.java
+++ server/src/main/java/com/CabBook/Cab/controller/Ridecontroller.java
@@ -1,10 +1,20 @@
 package com.CabBook.cab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.CabBook.cab.service.interfacefile.DriverService;
+import com.CabBook.cab.service.interfacefile.RideService;
 
 @RestController
 @RequestMapping("/api/rides")
 public class Ridecontroller {
+
+  @Autowired
+  private DriverService driverService;
+
+  @Autowired
+  private RideService rideService;
 
 }
