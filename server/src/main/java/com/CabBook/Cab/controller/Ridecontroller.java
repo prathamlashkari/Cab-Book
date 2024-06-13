212ea521ae664e9839876d413b4ed88a70101da8
Pratham <prathamlashkari841@gmail.com>
2024-06-13 22:01:43 +0530

    Adding dto files and folder

--- server/src/main/java/com/CabBook/Cab/controller/Ridecontroller.java
+++ server/src/main/java/com/CabBook/Cab/controller/Ridecontroller.java
@@ -1,11 +1,21 @@
 package com.CabBook.cab.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.CabBook.cab.DTO.RideDTO;
+import com.CabBook.cab.models.Ride;
+import com.CabBook.cab.models.User;
+import com.CabBook.cab.request.RideRequest;
 import com.CabBook.cab.service.interfacefile.DriverService;
 import com.CabBook.cab.service.interfacefile.RideService;
+import com.CabBook.cab.service.interfacefile.UserService;
 
 @RestController
 @RequestMapping("/api/rides")
@@ -16,5 +26,17 @@ public class Ridecontroller {
 
   @Autowired
   private RideService rideService;
+
+  @Autowired
+  private UserService userService;
+
+  @PostMapping
+  public ResponseEntity<RideDTO> userRequestRideHandler(@RequestBody RideRequest rideRequest,
+      @RequestHeader("Authorization") String jwt) {
+    User user = userService.findUserByToken(jwt);
+    Ride ride = rideService.requestRide(rideRequest, user);
+    RideDTO rideDto = DtoMapper.toRideDto(ride);
+    return new ResponseEntity<>(rideDto, HttpStatus.ACCEPTED);
+  }
 
 }
