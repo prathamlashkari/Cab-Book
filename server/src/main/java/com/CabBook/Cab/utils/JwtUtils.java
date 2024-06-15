847cc61709aa23373015e6eda6e132afebf7c5e1
Pratham <prathamlashkari841@gmail.com>
2024-06-15 10:30:40 +0530

    Added generate token methods in utility class

--- server/src/main/java/com/CabBook/Cab/utils/JwtUtils.java
+++ server/src/main/java/com/CabBook/Cab/utils/JwtUtils.java
@@ -1,5 +1,20 @@
 package com.CabBook.cab.utils;
 
+import java.util.Date;
+
+import org.springframework.security.core.Authentication;
+
+import com.CabBook.cab.config.JwtConstatnts;
+
+import io.jsonwebtoken.Jwts;
+
 public class JwtUtils {
+
+  public String generateJwtToken(Authentication authentication) {
+    String jwt = Jwts.builder().setIssuer("Code With Zosh")
+        .setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime() + 846000000))
+        .claim("email", authentication.getName()).signWith(JwtConstatnts.key).compact();
+    return jwt;
+  }
 
 }
