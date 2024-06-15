e24bd3b1d34e4863eb6afdb1b3d3e4c58845cb7b
Pratham <prathamlashkari841@gmail.com>
2024-06-15 10:09:55 +0530

    implementing dofilter with some modification

--- server/src/main/java/com/CabBook/Cab/config/JwtAuthenticationFilter.java
+++ server/src/main/java/com/CabBook/Cab/config/JwtAuthenticationFilter.java
@@ -1,21 +1,15 @@
 package com.CabBook.cab.config;
 
 import java.io.IOException;
-import java.util.Optional;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.CabBook.cab.utils.JwtUtils;
-
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -30,7 +24,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+
+    String jwt = request.getHeader(JwtConstatnts.JWT_HEADER);
+    jwt = jwt.substring(7);
+
+    if (jwt != null) {
+      SecretKey key = Keys.hmacShaKeyFor(JwtConstatnts.JWT_KEY.getBytes());
+    }
   }
 
 }
