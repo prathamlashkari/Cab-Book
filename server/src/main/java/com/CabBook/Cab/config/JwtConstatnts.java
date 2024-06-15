4163b562f39f800c9b323417bab30038f800c2c1
Pratham <prathamlashkari841@gmail.com>
2024-06-15 10:03:29 +0530

    Addes constatnt fiels

---
+++ server/src/main/java/com/CabBook/Cab/config/JwtConstatnts.java
@@ -0,0 +1,6 @@
+package com.CabBook.cab.config;
+
+public class JwtConstatnts {
+  public static final String JWT_KEY = "sakskfjiowennvmcnoiwpwfjspofmakmvqwevtwetqwetvwqrwvrewvwre";
+  public static final String JWT_HEADER = "Authorization";
+}
