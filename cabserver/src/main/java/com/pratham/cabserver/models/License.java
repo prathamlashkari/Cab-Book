5fc820d9982d0190876ed4e81f07467003ef1eee
Pratham <prathamlashkari841@gmail.com>
2024-06-16 10:36:09 +0530

    Added license models

--- cabserver/src/main/java/com/pratham/cabserver/models/License.java
+++ cabserver/src/main/java/com/pratham/cabserver/models/License.java
@@ -1,5 +1,20 @@
 package com.pratham.cabserver.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Document(collection = "license")
 public class License {
 
+  @Id
+  private String id;
+  private String licenseState;
+  private String licenseNumber;
+  private String licenseExpirationDate;
+  private String driverId;
 }
