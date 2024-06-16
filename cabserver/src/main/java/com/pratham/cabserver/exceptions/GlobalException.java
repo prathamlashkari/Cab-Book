d7d71dc575a3f710b8b17e43ba83c3d40107ab78
Pratham <prathamlashkari841@gmail.com>
2024-06-16 12:34:23 +0530

    Added all expcetions class

---
+++ cabserver/src/main/java/com/pratham/cabserver/exceptions/GlobalException.java
@@ -0,0 +1,38 @@
+package com.pratham.cabserver.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalException {
+
+  @ExceptionHandler(UserException.class)
+  public ResponseEntity<ErrorDetails> userExceptionHandler(UserException ue, WebRequest req) {
+    ErrorDetails err = new ErrorDetails(ue.getMessage(), req.getDescription(false), LocalDateTime.now());
+    return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(DriverException.class)
+  public ResponseEntity<ErrorDetails> DriverExceptionHandler(DriverException ue, WebRequest req) {
+    ErrorDetails err = new ErrorDetails(ue.getMessage(), req.getDescription(false), LocalDateTime.now());
+    return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(RideException.class)
+  public ResponseEntity<ErrorDetails> rideExceptionHandler(RideException ue, WebRequest req) {
+    ErrorDetails err = new ErrorDetails(ue.getMessage(), req.getDescription(false), LocalDateTime.now());
+    return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorDetails> otherExceptionHandler(Exception ue, WebRequest req) {
+    ErrorDetails err = new ErrorDetails(ue.getMessage(), req.getDescription(false), LocalDateTime.now());
+    return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+  }
+
+}
---
+++ cabserver/src/main/java/com/pratham/cabserver/exceptions/RideException.java
@@ -0,0 +1,7 @@
+package com.pratham.cabserver.exceptions;
+
+public class RideException extends Exception {
+  public RideException(String msg) {
+    super(msg);
+  }
+}
