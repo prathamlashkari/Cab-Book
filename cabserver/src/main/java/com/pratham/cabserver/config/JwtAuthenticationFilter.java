4e398b570119a66ef3071269704633a2638d9b1a
Pratham <prathamlashkari841@gmail.com>
2024-06-17 10:55:20 +0530

    Adding jwtautheticationfilterclass

---
+++ cabserver/src/main/java/com/pratham/cabserver/config/JwtAuthenticationFilter.java
@@ -0,0 +1,22 @@
+package com.pratham.cabserver.config;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Configuration
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+  }
+
+}
