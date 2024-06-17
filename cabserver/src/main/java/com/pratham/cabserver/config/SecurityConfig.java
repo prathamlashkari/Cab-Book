b1848ee8aab69fde78c1b0e34a34af0e2b6a6b7e
Pratham <prathamlashkari841@gmail.com>
2024-06-17 10:19:51 +0530

    Added password encodesr

--- cabserver/src/main/java/com/pratham/cabserver/config/SecurityConfig.java
+++ cabserver/src/main/java/com/pratham/cabserver/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.Nullable;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -47,6 +49,11 @@ public class SecurityConfig {
 
     return https.build();
 
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
