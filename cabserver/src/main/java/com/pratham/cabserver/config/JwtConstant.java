5a2ea9fdb16d349f251610686778954f08e81579
Pratham <prathamlashkari841@gmail.com>
2024-06-17 10:57:22 +0530

    Added jwt constants

---
+++ cabserver/src/main/java/com/pratham/cabserver/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.pratham.cabserver.config;
+
+public class JwtConstant {
+  public static final String key = "sfpqiewramdncvposehowfnsdjkfnowqepoewmnskasskljdeesf";
+  public static final String header = "Authorization";
+
+}
