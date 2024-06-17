8b4e02f429836cd47fc1ddd347a3c0157577d4f8
Pratham <prathamlashkari841@gmail.com>
2024-06-17 09:56:43 +0530

    Added secirty filter chain

---
+++ cabserver/src/main/java/com/pratham/cabserver/config/SecurityFilterChain.java
@@ -0,0 +1,15 @@
+package com.pratham.cabserver.config;
+
+import java.net.http.HttpRequest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SecurityFilterChain {
+
+  public SecurityFilterChain securityFilterChain(HttpSecurity https) throws Exception {
+    https.
+    return https.build();
+  }
+}
