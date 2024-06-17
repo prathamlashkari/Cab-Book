4c5a529a638b55154605093d38d2fa80a33458a6
Pratham <prathamlashkari841@gmail.com>
2024-06-17 12:03:11 +0530

    filter created

--- cabserver/src/main/java/com/pratham/cabserver/config/JwtAuthenticationFilter.java
+++ cabserver/src/main/java/com/pratham/cabserver/config/JwtAuthenticationFilter.java
@@ -1,10 +1,19 @@
 package com.pratham.cabserver.config;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,7 +26,21 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+    String jwt = request.getHeader(JwtConstant.Jwt_Header);
+    if (jwt != null) {
+      try {
+        jwt = jwt.substring(7);
+        Claims claims = Jwts.parserBuilder().setSigningKey(JwtConstant.key).build().parseClaimsJws(jwt).getBody();
+        String email = String.valueOf(claims.get("email"));
+        String authorities = (String) claims.get("authorities");
+        List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, auths);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+      } catch (Exception e) {
+        throw new BadCredentialsException("Invalid token received... erros");
+      }
+
+    }
   }
 
 }
