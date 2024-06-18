c25e5cbcf6683d39024b8589fad2cae4adcb15bd
Pratham <prathamlashkari841@gmail.com>
2024-06-18 19:42:13 +0530

    Creatig utils class

--- cabserver/src/main/java/com/pratham/cabserver/utils/JwtUtils.java
+++ cabserver/src/main/java/com/pratham/cabserver/utils/JwtUtils.java
@@ -1,5 +1,14 @@
 package com.pratham.cabserver.utils;
 
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
+
+import io.jsonwebtoken.Jwts;
+
 public class JwtUtils {
+
+  public String generaJwtToken(Authentication authentication)
+  {
+    String jwt = Jwts.builder().setIssuer("pratham")
+  }
 
 }
