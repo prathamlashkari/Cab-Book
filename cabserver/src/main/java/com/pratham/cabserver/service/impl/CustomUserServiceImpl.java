1f4bbadcc3812f0c151a08fd41d10609fd984f0c
Pratham <prathamlashkari841@gmail.com>
2024-06-18 20:01:47 +0530

    user detail for driver

--- cabserver/src/main/java/com/pratham/cabserver/service/impl/CustomUserServiceImpl.java
+++ cabserver/src/main/java/com/pratham/cabserver/service/impl/CustomUserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.pratham.cabserver.service.impl;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -9,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.pratham.cabserver.models.Driver;
+import com.pratham.cabserver.models.User;
 import com.pratham.cabserver.repository.DriverRepository;
 import com.pratham.cabserver.repository.UserRepository;
 
@@ -21,9 +25,13 @@ public class CustomUserServiceImpl implements UserDetailsService {
   private UserRepository userRepository;
 
   @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    List<GrantedAuthority> authorities = new ArrayList<>();
+    User user = userRepository.findByEmail(email);
+    if (user != null) {
+      return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+    Driver driver = driverRepository.findByEmail(email);
   }
 
 }
