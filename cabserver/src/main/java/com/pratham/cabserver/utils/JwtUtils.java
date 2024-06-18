1010eefeda9b012acecce344e1732e1c6e946050
Pratham <prathamlashkari841@gmail.com>
2024-06-18 19:44:37 +0530

    generateToken added

--- cabserver/src/main/java/com/pratham/cabserver/utils/JwtUtils.java
+++ cabserver/src/main/java/com/pratham/cabserver/utils/JwtUtils.java
@@ -1,14 +1,22 @@
 package com.pratham.cabserver.utils;
 
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
+import java.util.Date;
+
+import org.springframework.security.core.Authentication;
+
+import com.pratham.cabserver.config.JwtConstant;
 
 import io.jsonwebtoken.Jwts;
 
 public class JwtUtils {
 
-  public String generaJwtToken(Authentication authentication)
-  {
+  public String generaJwtToken(Authentication authentication) {
     String jwt = Jwts.builder().setIssuer("pratham")
+        .setExpiration(new Date(new Date().getTime() + 864000000))
+        .claim("email", authentication.getName())
+        .signWith(JwtConstant.key)
+        .compact();
+    return jwt;
   }
 
 }
