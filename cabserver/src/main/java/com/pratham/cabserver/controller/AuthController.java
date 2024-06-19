06077c051165aef0d20e932ba50d09d401f1c9ae
Pratham <prathamlashkari841@gmail.com>
2024-06-19 11:55:31 +0530

    aded login handler now verifying it

--- cabserver/src/main/java/com/pratham/cabserver/controller/AuthController.java
+++ cabserver/src/main/java/com/pratham/cabserver/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pratham.cabserver.enums.UserRole;
 import com.pratham.cabserver.exceptions.UserException;
+import com.pratham.cabserver.request.LoginRequest;
 import com.pratham.cabserver.request.SignupRequest;
 import com.pratham.cabserver.response.JwtResponse;
 import com.pratham.cabserver.service.AuthService;
+import com.pratham.cabserver.service.impl.CustomUserServiceImpl;
 import com.pratham.cabserver.utils.JwtUtils;
 
 @RestController
@@ -25,6 +28,8 @@ public class AuthController {
   private AuthService authService;
   @Autowired
   private JwtUtils jwtUtils;
+  @Autowired
+  private CustomUserServiceImpl customUserServiceImpl;
 
   @PostMapping("/signup")
   public ResponseEntity<JwtResponse> signupHandler(@RequestBody SignupRequest req) throws UserException {
@@ -41,7 +46,17 @@ public class AuthController {
     jwtResponse.setRole(UserRole.USER);
 
     return new ResponseEntity<>(jwtResponse, HttpStatus.OK);
+  }
 
+  public ResponseEntity<JwtResponse> loginHandler(@RequestBody LoginRequest req) {
+    String email = req.getEmail();
+    String password = req.getPassword();
+    Authentication authentication = Authe
+  }
+
+  private Authentication authenticate(String password , String email)
+  {
+    UserDetails userDetails = 
   }
 
 }
