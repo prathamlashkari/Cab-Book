1dbfebdb32e587e91ae84d341d0d5de8d04c0f48
Pratham <prathamlashkari841@gmail.com>
2024-06-19 09:34:30 +0530

    integrate signup with controller

--- cabserver/src/main/java/com/pratham/cabserver/controller/AuthController.java
+++ cabserver/src/main/java/com/pratham/cabserver/controller/AuthController.java
@@ -1,10 +1,27 @@
 package com.pratham.cabserver.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pratham.cabserver.request.SignupRequest;
+import com.pratham.cabserver.response.JwtResponse;
+import com.pratham.cabserver.service.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+  @Autowired
+  private AuthService authService;
+
+  @PostMapping("/signup")
+  public ResponseEntity<JwtResponse> signupHandler(@RequestBody SignupRequest req) {
+
+  }
 
 }
--- cabserver/src/main/java/com/pratham/cabserver/service/impl/AuthServiceImpl.java
+++ cabserver/src/main/java/com/pratham/cabserver/service/impl/AuthServiceImpl.java
@@ -3,7 +3,6 @@ package com.pratham.cabserver.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
