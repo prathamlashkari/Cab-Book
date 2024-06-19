920795238b723351ebf8713b06c13ca965ffb940
Pratham <prathamlashkari841@gmail.com>
2024-06-19 12:17:21 +0530

    Added calculatro class

---
+++ cabserver/src/main/java/com/pratham/cabserver/utils/Calculator.java
@@ -0,0 +1,35 @@
+package com.pratham.cabserver.utils;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+
+  private static final int Earth_Radius = 6371;
+
+  public double calculateDistance(double sourceLat, double sourceLng, double desLat, double desLng) {
+    double dLat = Math.toRadians(desLat - sourceLat);
+    double dLng = Math.toRadians(desLng - sourceLng);
+
+    double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(Math.toRadians(sourceLat)) * Math.cos(Math.toRadians(desLat)) * Math.sin(dLng / 2) *
+            Math.sin(dLng / 2);
+    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    double distance = Earth_Radius * c;
+    return distance;
+  }
+
+  public long calculateDuration(LocalDateTime startTime, LocalDateTime endTime) {
+    Duration duration = Duration.between(startTime, endTime);
+    return duration.getSeconds();
+  }
+
+  public double calculateFare(double distance) {
+    double baseFare = 11;
+    double totalFare = baseFare * distance;
+    return totalFare;
+  }
+}
