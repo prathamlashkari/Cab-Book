0cf111979e757cf9e930c2acbf8680424aa2c51a
Pratham <prathamlashkari841@gmail.com>
2024-06-19 12:30:08 +0530

    Added home controller

--- cabserver/src/main/java/com/pratham/cabserver/controller/HomeController.java
+++ cabserver/src/main/java/com/pratham/cabserver/controller/HomeController.java
@@ -1,5 +1,19 @@
 package com.pratham.cabserver.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pratham.cabserver.response.MessageResponse;
+
+@RestController
 public class HomeController {
+
+  @GetMapping("/")
+  public ResponseEntity<MessageResponse> homeHandler() {
+    MessageResponse res = new MessageResponse("Wecome to backend");
+    return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+  }
 
 }
