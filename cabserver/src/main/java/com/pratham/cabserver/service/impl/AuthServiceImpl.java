65cc52b09a6a5a70b3c9e32f8e55ff92b432f184
Pratham <prathamlashkari841@gmail.com>
2024-06-19 09:27:12 +0530

    modifying signup handnler

--- cabserver/src/main/java/com/pratham/cabserver/service/impl/AuthServiceImpl.java
+++ cabserver/src/main/java/com/pratham/cabserver/service/impl/AuthServiceImpl.java
@@ -1,8 +1,13 @@
 package com.pratham.cabserver.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.pratham.cabserver.exceptions.UserException;
+import com.pratham.cabserver.models.User;
+import com.pratham.cabserver.repository.UserRepository;
 import com.pratham.cabserver.request.LoginRequest;
 import com.pratham.cabserver.request.SignupRequest;
 import com.pratham.cabserver.service.AuthService;
@@ -10,12 +15,29 @@ import com.pratham.cabserver.service.AuthService;
 @Service
 public class AuthServiceImpl implements AuthService {
 
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   @Override
   public Authentication signUp(SignupRequest req) throws Exception {
     String email = req.getEmail();
     String fullname = req.getName();
     String mobile = req.getMobile();
     String password = req.getPassword();
+
+    User user = userRepository.findByEmail(email);
+    if (user != null) {
+      throw new UserException("User already exists witht this email");
+    }
+    User newUser = new User();
+    newUser.setEmail(email);
+    newUser.setPassword(passwordEncoder.encode(password));
+    newUser.setFullName(fullname);
+    newUser.setMobile(mobile);
+    User createdUser = userRepository.save(newUser);
+
   }
 
   @Override
